895ce4d14db9926915e7ad6299a47877528c4bb2
darinson perea <darinson.perea@globant.com>
2024-06-17 12:49:45 -0500

    disabling example

--- src/context/ContactsContext.tsx
+++ src/context/ContactsContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { useContactsClient } from "../features/contacts/useContactsClient";
-import { Contacts } from "../redux/slices/contactsSlice";
+// import { createContext, useContext, useEffect, useState } from "react";
+// import { useContactsClient } from "../features/contacts/useContactsClient";
+// import { Contacts } from "../redux/slices/contactsSlice";
 
 // interface Contacts {
 //   id: number;
@@ -11,47 +11,47 @@ import { Contacts } from "../redux/slices/contactsSlice";
 //   favorite: boolean;
 // }
 
-const ContactsContext = createContext<Contacts[]>([]);
+// const ContactsContext = createContext<Contacts[]>([]);
 
-function ContactsProvider({ children }: { children: React.ReactNode }) {
-  const [contacts, setContacts] = useState<Contacts[]>([]);
-  const { data } = useContactsClient();
+// function ContactsProvider({ children }: { children: React.ReactNode }) {
+//   const [contacts, setContacts] = useState<Contacts[]>([]);
+//   const { data } = useContactsClient();
 
-  useEffect(() => {
-    setContacts(
-      data?.map((contact: Contacts) => ({ ...contact, favorite: false }))
-    );
-  }, [data]);
+//   useEffect(() => {
+//     setContacts(
+//       data?.map((contact: Contacts) => ({ ...contact, favorite: false }))
+//     );
+//   }, [data]);
 
-  function onDelete(id: number) {
-    setContacts(contacts?.filter((contact) => contact.id !== id));
-  }
+//   function onDelete(id: number) {
+//     setContacts(contacts?.filter((contact) => contact.id !== id));
+//   }
 
-  function onLiked(id: number) {
-    const contact = contacts?.find((contact) => contact.id === id);
-    if (contact) contact.favorite = !contact.favorite;
+//   function onLiked(id: number) {
+//     const contact = contacts?.find((contact) => contact.id === id);
+//     if (contact) contact.favorite = !contact.favorite;
 
-    return setContacts([...contacts]);
-  }
+//     return setContacts([...contacts]);
+//   }
 
-  const favorites = contacts?.filter((contact) => contact.favorite === true);
+//   const favorites = contacts?.filter((contact) => contact.favorite === true);
 
-  return (
-    <ContactsContext.Provider
-      value={{ contacts, favorites, onDelete, onLiked }}
-    >
-      {children}
-    </ContactsContext.Provider>
-  );
-}
+//   return (
+//     <ContactsContext.Provider
+//       value={{ contacts, favorites, onDelete, onLiked }}
+//     >
+//       {children}
+//     </ContactsContext.Provider>
+//   );
+// }
 
-export function useContacts() {
-  const context = useContext(ContactsContext);
+// export function useContacts() {
+//   const context = useContext(ContactsContext);
 
-  if (context === "undefined")
-    throw new Error("The provider was used outside the children");
+//   if (context === "undefined")
+//     throw new Error("The provider was used outside the children");
 
-  return context;
-}
+//   return context;
+// }
 
-export default ContactsProvider;
+// export default ContactsProvider;
