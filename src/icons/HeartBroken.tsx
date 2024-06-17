cd039f7d7712f88c0e940178304fb6b476079f93
darinson perea <darinson.perea@globant.com>
2024-06-17 12:37:23 -0500

    icons

---
+++ src/icons/HeartBroken.tsx
@@ -0,0 +1,21 @@
+function HeartBroken() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-heart-broken"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+      <path d="M12 6l-2 4l4 3l-2 4v3" />
+    </svg>
+  );
+}
+
+export default HeartBroken;
