200eab0b7d0cb994804a86a3a4b532446761077d
darinson perea <darinson.perea@globant.com>
2024-06-24 09:55:10 -0500

    config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "selector",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
@@ -13,6 +14,7 @@ export default {
         "red-light": "#cf6679",
         "custom-black": "#231f20",
         "black-light": "#00000090",
+        "mode-black": "#2d2e2d",
       },
       gridTemplateColumns: {
         title: "max-content 1fr",
@@ -23,6 +25,12 @@ export default {
         header: "0 0px 10px 0 hsl(220, 14%, 75%)",
         "hover-card": "0px 2px 10px 1px #f7f7f6",
         card: "0px 4px 10px 0px #e6e6e6",
+      },
+      width: {
+        "custom-screen": "80vw",
+      },
+      minHeight: {
+        "custom-height": "94.1vh",
       },
     },
   },
