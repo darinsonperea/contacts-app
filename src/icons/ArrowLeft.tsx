3f19cb47924f145d7c43ae2e0eb9e961c56de7c7
darinson perea <darinson.perea@globant.com>
2024-06-24 09:54:05 -0500

    adding icons

---
+++ src/icons/ArrowLeft.tsx
@@ -0,0 +1,22 @@
+function ArrowLeft() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-left"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 12l14 0" />
+      <path d="M5 12l4 4" />
+      <path d="M5 12l4 -4" />
+    </svg>
+  );
+}
+
+export default ArrowLeft;
