24cc8ef0611100fcff93a461f0e158bb8152f89f
darinson perea <darinson.perea@globant.com>
2024-06-24 09:56:13 -0500

    config of toaster and some adding dark mode provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+import store from "./redux/store";
+import DarkModeProvider from "./context/DarkModeContext";
 import Layout from "./ui/Layout";
 import Overview from "./pages/Overview";
 import Contacts from "./pages/Contacts";
 import Favorites from "./pages/Favorites";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,8 +26,14 @@ function App() {
           <ReactQueryDevtools />
           <BrowserRouter>
             <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<Overview />} />
+              <Route
+                element={
+                  <DarkModeProvider>
+                    <Layout />
+                  </DarkModeProvider>
+                }
+              >
+                <Route path="/" element={<Navigate to="/overview" replace />} />
                 <Route path="/overview" element={<Overview />} />
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/favorites" element={<Favorites />} />
@@ -33,6 +41,27 @@ function App() {
               </Route>
             </Routes>
           </BrowserRouter>
+          <Toaster
+            position="top-right"
+            gutter={8}
+            containerStyle={{
+              margin: "50px 0",
+            }}
+            toastOptions={{
+              success: {
+                duration: 3000,
+              },
+              error: {
+                duration: 5000,
+              },
+              style: {
+                fontSize: "14px",
+                maxWidth: "500px",
+                padding: "10px",
+                color: "black",
+              },
+            }}
+          />
         </QueryClientProvider>
       </Provider>
     </>
