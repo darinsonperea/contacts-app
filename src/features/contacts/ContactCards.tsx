ff43e48a874459ccb0c325c913eec2b77bbf391f
darinson perea <darinson.perea@globant.com>
2024-06-24 10:02:16 -0500

    changes in props

--- src/features/contacts/ContactCards.tsx
+++ src/features/contacts/ContactCards.tsx
@@ -1,42 +1,63 @@
+// import { useEffect } from "react";
 import Heart from "../../icons/Heart";
 import HeartBroken from "../../icons/HeartBroken";
 import Trash from "../../icons/Trash";
-import {
-  getContactsSlice,
-  liked,
-  remove,
-} from "../../redux/slices/contactsSlice";
+import { useDelete } from "../../services/hooks/useDelete";
+import { useToggleLike } from "../../services/hooks/useToggleLike";
 import Card from "../../ui/Card";
-// import ErrorMessage from "../../ui/ErrorMessage";
-import { useDispatch, useSelector } from "react-redux";
+import Pagination from "../../ui/Pagination";
+import { CardsProps } from "../../utils/types";
+// import { editContact } from "../../services/apiContacts";
 
-function ContactCards() {
-  const dispatch = useDispatch();
-  const contacts = useSelector(getContactsSlice);
+function ContactCards({ flag, contacts, count }: CardsProps) {
+  const { deleteContact, isPending: isDeleting } = useDelete();
+  const { toggleLike } = useToggleLike();
 
-  // if (error) return <ErrorMessage
+  // useEffect(() => {
+  //   async function test() {
+  //     console.log(
+  //       await editContact({
+  //         id: 40,
+  //         name: "Maria",
+  //         lastName: "Eugenia",
+  //         email: "maria.eugenia@gmail.com",
+  //       })
+  //     );
+  //   }
+  //   test();
+  // }, []);
 
   return (
-    <section className="w-[80vw] mx-auto gap-4 md:gap-x-10 md:gap-y-14 grid grid-cols-dynamic justify-center">
-      {contacts?.map((contact) => (
-        <Card contact={contact} key={contact.id}>
-          <button
-            className={`${contact.favorite ? "text-red-700" : "text-green-light"}`}
-            onClick={() => dispatch(liked(contact.id))}
-          >
-            {contact.favorite ? <HeartBroken /> : <Heart />}
-          </button>
-          <button
-            className="text-red-700"
-            onClick={() => {
-              dispatch(remove(contact.id));
-            }}
-          >
-            <Trash />
-          </button>
-        </Card>
-      ))}
-    </section>
+    <>
+      <section className="w-[80vw] mx-auto gap-4 md:gap-x-10 md:gap-y-14 grid grid-cols-dynamic justify-center">
+        {contacts?.map((contact) => (
+          <Card contact={contact} key={contact.id}>
+            <button
+              className={`${contact.favorite ? "text-red-700" : "text-green-light"} disabled:cursor-not-allowed`}
+              onClick={() =>
+                toggleLike({ id: contact.id, favorite: !contact.favorite })
+              }
+              disabled={isDeleting}
+            >
+              {contact.favorite ? <HeartBroken /> : <Heart />}
+            </button>
+
+            {flag && (
+              <button
+                className="text-red-700 disabled:cursor-not-allowed"
+                onClick={() => {
+                  deleteContact(contact.id);
+                }}
+                disabled={isDeleting}
+              >
+                <Trash />
+              </button>
+            )}
+          </Card>
+        ))}
+      </section>
+      <Pagination count={count} />
+    </>
   );
 }
 
--- src/features/favorites/FavoriteCards.tsx
+++ src/features/favorites/FavoriteCards.tsx
@@ -1,28 +1,29 @@
 import Card from "../../ui/Card";
 import HeartBroken from "../../icons/HeartBroken";
-// import ErrorMessage from "../../ui/ErrorMessage";
-import { useDispatch, useSelector } from "react-redux";
-import { getFavoritesSlice, liked } from "../../redux/slices/contactsSlice";
+import { useToggleLike } from "../../services/hooks/useToggleLike";
+import { CardsProps } from "../../utils/types";
+import Pagination from "../../ui/Pagination";
 
-function FavoriteCards() {
-  const dispatch = useDispatch();
-  const favorites = useSelector(getFavoritesSlice);
-
-  // if (error) return <ErrorMessage />;
+function FavoriteCards({ contacts: favorites, count }: CardsProps) {
+  const { toggleLike, isPending } = useToggleLike();
 
   return (
-    <section className="w-[80vw] mx-auto gap-4 md:gap-x-10 md:gap-y-14 grid grid-cols-dynamic justify-center">
-      {favorites?.map((contact) => (
-        <Card contact={contact} key={contact.id}>
-          <button
-            className="text-red-700"
-            onClick={() => dispatch(liked(contact.id))}
-          >
-            <HeartBroken />
-          </button>
-        </Card>
-      ))}
-    </section>
+    <>
+      <section className="w-[80vw] mx-auto gap-4 md:gap-x-10 md:gap-y-14 grid grid-cols-dynamic justify-center">
+        {favorites?.map((contact) => (
+          <Card contact={contact} key={contact.id}>
+            <button
+              className="text-red-700"
+              onClick={() => toggleLike({ id: contact.id, favorite: false })}
+              disabled={isPending}
+            >
+              <HeartBroken />
+            </button>
+          </Card>
+        ))}
+      </section>
+      <Pagination count={count} />
+    </>
   );
 }
 
