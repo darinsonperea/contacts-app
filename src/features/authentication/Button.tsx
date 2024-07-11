006d00bfe6ddcf6492519a4720730e17e510f51d
darinson perea <darinson.perea@globant.com>
2024-07-11 09:29:11 -0500

    making no reusable components

---
+++ src/features/authentication/Button.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  width: max-content;
+  padding: 4px 24px;
+  border-radius: 10px;
+  border: 1px solid;
+  margin-top: 24px;
+  color: var(--text--color);
+`;
+
+export default StyledButton;
---
+++ src/features/authentication/Input.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  outline: none;
+  width: 100%;
+  padding: 12px;
+  border-radius: 50px;
+  background-color: var(--green--form);
+
+  &::placeholder {
+  }
+`;
+
+export default StyledInput;
