42d6d4ab710c3fc47fd845ae9e6a40ceb94e2c93
Darinson Perea <darinjrper@gmail.com>
2024-07-14 12:57:23 -0500

    refactoring

--- src/features/authentication/hooks/useLogout.ts
+++ src/features/authentication/hooks/useLogout.ts
@@ -1,15 +1,29 @@
+import { useContacts } from "../../../services/hooks/useContacts";
+// import useFetch from "../../../hooks/useFetch";
+// import { headersSupabase } from "../../../services/supabase";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout as logoutApi } from "../../../services/apiAuth";
 
 export default function useLogout() {
+  const { refetch } = useContacts();
+
+  // const { mutate: logout } = useFetch({
+  //   url: "https://dwnavszoazxzffdtrhhm.supabase.co/auth/v1/logout?scope=global",
+  //   method: "POST",
+  //   headers: {
+  //     Authorization:
+  //       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3bmF2c3pvYXp4emZmZHRyaGhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4MDQ2MjEsImV4cCI6MjAzNDM4MDYyMX0.Tadj3eCkgU9QWjiRNIxsSyZgmgqr246TbQzsfegE-Mg",
+  //     ...headersSupabase,
+  //   },
+  // });
+
   const queryClient = useQueryClient();
 
   const { mutate: logout } = useMutation({
     mutationFn: logoutApi,
-    onSuccess: (data) => {
+    onSuccess: () => {
       queryClient.removeQueries();
-
-      console.log(data);
+      refetch();
     },
   });
 
--- src/services/apiContacts.ts
+++ src/services/apiContacts.ts
@@ -1,41 +1,8 @@
-import { ContactWithoutId, EditContactType } from "../utils/types";
+import { EditContactType } from "../utils/types";
 import { supabase, supabaseUrl } from "./supabase";
 
 const storagePath = "storage/v1/object/public/avatars";
 const savedPath = `${supabaseUrl}/${storagePath}`;
-
-export async function getContacts() {
-  const { data, error } = await supabase.from("contacts").select("*");
-  // .eq("userId", "");
-
-  // if (page) {
-  //   const from = (page - 1) * PAGE_SIZE;
-  //   const to = from + PAGE_SIZE - 1;
-  //   query = query.range(from, to);
-  // }
-
-  if (error) throw new Error(error.message);
-
-  return { data };
-}
-
-export async function createContact(newContact: ContactWithoutId) {
-  const imageName = `${Math.random()}-${newContact?.avatar?.name}`.replace(
-    "/",
-    ""
-  );
-  const imagePath = `${savedPath}/${imageName}`;
-  uploadImageToStorage(newContact?.avatar, imageName);
-
-  const { data, error } = await supabase
-    .from("contacts")
-    .insert([{ ...newContact, avatar: imagePath }])
-    .select();
-
-  if (error) throw new Error(error.message);
-
-  return data;
-}
 
 export async function editContact(contactToEdit: EditContactType) {
   const { data, error } = await supabase
@@ -49,42 +16,9 @@ export async function editContact(contactToEdit: EditContactType) {
   return data;
 }
 
-export async function toggleLike({
-  id,
-  favorite,
-}: {
-  id: number;
-  favorite: boolean;
-}) {
-  const { data, error } = await supabase
-    .from("contacts")
-    .update({ favorite })
-    .eq("id", id)
-    .select();
-
-  if (error) throw new Error(error.message);
-
-  return data;
-}
-
-export async function deleteContact({
-  id,
-  imagePath,
-}: {
-  id: number;
-  imagePath: string;
-}) {
-  const { error } = await supabase.from("contacts").delete().eq("id", id);
-
-  if (error) throw new Error(error.message);
-
-  const imageName = imagePath.substring(savedPath.length + 1);
-  deleteImageFromStorage(imageName);
-}
-
 // Bucket actions
 
-async function uploadImageToStorage(image: File, imageName: string) {
+export async function uploadImageToStorage(image: File, imageName: string) {
   const { error } = await supabase.storage
     .from("avatars")
     .upload(imageName, image);
@@ -92,6 +26,7 @@ async function uploadImageToStorage(image: File, imageName: string) {
   if (error) throw new Error(error.message);
 }
 
-async function deleteImageFromStorage(imageName: string) {
+export async function deleteImageFromStorage(imagePath: string) {
+  const imageName = imagePath.substring(savedPath.length + 1);
   await supabase.storage.from("avatars").remove([imageName]);
 }
