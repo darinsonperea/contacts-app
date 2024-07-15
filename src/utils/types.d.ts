b41b2003d9b3e78aaaf4c6a783ac677f4e371b22
Darinson Perea <darinjrper@gmail.com>
2024-07-15 12:44:30 -0500

    updating types

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,17 +1,44 @@
 type Methods = "POST" | "PUT" | "PATCH" | "DELETE";
-export type ContactWithoutId = Omit<Contacts, "id">;
+export type ContactWithoutId = Omit<ContactDataType, "id">;
 
-export interface Contacts {
+type CustomFile = {
+  name: string;
+  size: number;
+  lastModified: number;
+  type: string;
+  webkitRelativePath: string;
+  lastModifiedDate: object;
+};
+
+export interface ContactDataType {
   id: number;
   name: string;
   lastName: string;
   email: string;
-  avatar: string | File;
+  avatar: unknown | FileList;
+  favorite: boolean;
+}
+
+export interface DefaultContact {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+  favorite: boolean;
+}
+
+export interface ContactsTypes {
+  id: number;
+  name: string;
+  lastName: string;
+  email: string;
+  avatar: string;
   favorite: boolean;
 }
 
 export type InitialData = {
-  contacts: Contacts[];
+  contacts: ContactDataType[];
   isOpen: boolean;
 };
 
@@ -31,7 +58,7 @@ export interface Gender {
 
 export interface CardsProps {
   flag?: boolean;
-  contacts: Contacts[] | undefined;
+  contacts: ContactsTypes[] | undefined;
 }
 
 export type ThemeContextType = {
@@ -40,8 +67,8 @@ export type ThemeContextType = {
 };
 
 export interface AuthContextType {
-  manageGetContacts: () => Contacts[] | undefined;
-  manageGetFavorites: () => Contacts[] | undefined;
+  manageGetContacts: () => ContactTypes[] | undefined;
+  manageGetFavorites: () => ContactTypes[] | undefined;
   manageCreateContact: (newContact: ContactWithoutId) => void;
   manageDeleteContact: (id: number, imagePath?: string) => void;
   manageToggleLike: (id: number, favorite: boolean) => void;
@@ -51,7 +78,6 @@ export interface AuthContextType {
 export interface LoginTypes {
   email: string;
   password: string;
-  confirmPassword: string;
 }
 
 export interface FetchTypes {
@@ -65,3 +91,49 @@ export interface FetchTypes {
 }
 
 export type QueryTypes = Pick<FetchTypes, "url" | "headers">;
+
+// Auth types
+
+export interface AppMetadata {
+  provider: string;
+  providers: string[];
+}
+
+export interface Identity {
+  identity_id: string;
+  id: string;
+  user_id: string;
+  identity_data: Data;
+  provider: string;
+  last_sign_in_at: Date;
+  created_at: Date;
+  updated_at: Date;
+  email: string;
+}
+
+export interface Data {
+  email: string;
+  email_verified: boolean;
+  lastName: string;
+  name: string;
+  phone_verified: boolean;
+  sub: string;
+}
+
+export interface AuthInfo {
+  id: string;
+  aud: string;
+  role: string;
+  email: string;
+  email_confirmed_at: Date;
+  phone: string;
+  confirmation_sent_at: Date;
+  confirmed_at: Date;
+  last_sign_in_at: Date;
+  app_metadata: AppMetadata;
+  user_metadata: Data;
+  identities: Identity[];
+  created_at: Date;
+  updated_at: Date;
+  is_anonymous: boolean;
+}
