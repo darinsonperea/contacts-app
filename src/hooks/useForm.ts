9243e2eeafcad6778ab8a80773c215a982b7121e
Darinson Perea <darinjrper@gmail.com>
2024-07-15 12:48:19 -0500

    disabled

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,19 +1,16 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 function useForm() {
-  const [value, setValue] = useState("");
-
-  function register() {
-    // setValue(event.target.value);
-
-    return {
-      value: value,
-    };
-  }
-
-  return {
-    register,
-  };
+  // const [value, setValue] = useState("");
+  // function register() {
+  //   // setValue(event.target.value);
+  //   return {
+  //     value: value,
+  //   };
+  // }
+  // return {
+  //   register,
+  // };
 }
 
 export default useForm;
